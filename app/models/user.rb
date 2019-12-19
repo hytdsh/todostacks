ce6d31b78ab1dc7d4da8194ae78a05f9219c1c8b
hytdsh <hytdsh@gmail.com>
2019-12-19 10:43:21 +0900

    OmniAuthからインスタンスを生成するのはコストが高め、という指摘を受けてOAuth2.Clientから生成することにした。

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,13 +17,15 @@ class User < ApplicationRecord
 
   def refresh_access_token_if_expired!
     if expires_at < Time.now.to_i
-      oauth_with_google_urls = OmniAuth::Strategies::GoogleOauth2.new(
-        nil,
+      # 'site:' and 'token_url' come from https://accounts.google.com/.well-known/openid-configuration
+      oauth_client = OAuth2::Client.new(
         Rails.application.credentials.google[:client_id],
-        Rails.application.credentials.google[:client_secret]
+        Rails.application.credentials.google[:client_secret],
+        site: 'https://oauth2.googleapis.com',
+        token_url: '/token'
       )
       oauth_access_token = OAuth2::AccessToken.new(
-        oauth_with_google_urls.client,
+        oauth_client,
         token,
         refresh_token: refresh_token,
         expires_at: expires_at
