4f7d9069d5afa26bd41d408a303cf52bc4196ab2
hytdsh <hytdsh@gmail.com>
2019-12-20 15:08:34 +0900

    ついでにGoogle::Apis::TasksV1::TasksService.newする場所など更新する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,20 @@ class ApplicationController < ActionController::Base
     def refresh_access_token_if_expired!
       current_user.refresh_access_token_if_expired!
     end
+
+    def connect_google_tasks
+      if user_signed_in?
+        oauth_client = Google::Auth::UserRefreshCredentials.new(
+          {
+            "access_token" => current_user.token,
+            "refresh_token" => current_user.refresh_token,
+            "expires_at" => current_user.expires_at,
+            "client_id" => Rails.application.credentials.google[:client_id],
+            "client_secret" => Rails.application.credentials.google[:client_secret]
+          }
+        )
+        @service = Google::Apis::TasksV1::TasksService.new
+        @service.authorization = oauth_client
+      end
+    end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,31 +1,10 @@
-require 'google/apis/tasks_v1'
-require 'googleauth'
-
 class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:main]
   before_action :refresh_access_token_if_expired!, only: [:main]
-
-  Tasks = Google::Apis::TasksV1
+  before_action :connect_google_tasks, only: [:main]
 
   def main
-    if user_signed_in?
-      oauth_client = Google::Auth::UserRefreshCredentials.new(
-#      oauth = Google::Auth::Credentials.new( # does not work with error "no implicit conversion of nil into String"
-        {
-          "access_token" => current_user.token,
-          "refresh_token" => current_user.refresh_token,
-          "expires_at" => current_user.expires_at,
-          "client_id" => Rails.application.credentials.google[:client_id],
-          "client_secret" => Rails.application.credentials.google[:client_secret]
-        }
-      )
-
-      service = Tasks::TasksService.new
-      service.authorization = oauth_client
-#      service.authorization = oauth.client # for Google::Auth::Credentials (if it works)
-
-      @response = service.list_tasklists # max_results: 10 というような指定もできる
-    end
+    @response = @service.list_tasklists # max_results: 10 というような指定もできる
   end
 
   def help
