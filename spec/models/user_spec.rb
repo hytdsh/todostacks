f95f360a364dc25ee9a038fec6ed7110a82a803b
hytdsh <hytdsh@gmail.com>
2020-01-15 14:47:27 +0900

    ひとまず通るテストにした

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "add some examples to (or delete) #{__FILE__}" do
+  end
 end
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,10 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
-  describe "GET /pages" do
-    it "works! (now write some real specs)" do
-      get pages_index_path
-      expect(response).to have_http_status(200)
-    end
+  it "認証が何もないと / へのアクセスが 302 でリダイレクトされる" do
+    get root_path
+    expect(response).to have_http_status(302)
+    expect(response).to redirect_to new_user_session_path
+  end
+
+  it "GoogleのOAuth認証を通過していれば / へのアクセスが 200 で応答する" do
+    # このテストは system spec に書くべきか？
+  end
+
+  it "GET /help" do
+    get help_path
+    expect(response).to have_http_status(200)
+  end
+
+  it "GET /contact" do
+    get contact_path
+    expect(response).to have_http_status(200)
+  end
+
+  it "GET /about" do
+    get about_path
+    expect(response).to have_http_status(200)
   end
 end
