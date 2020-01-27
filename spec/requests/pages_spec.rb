230ee83372d027713654830d30ff86769be91d1e
hytdsh <hytdsh@gmail.com>
2020-01-27 13:33:05 +0900

    byebugで追跡している振る舞いについての推測を書き込んでおく

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -15,7 +15,29 @@ RSpec.describe "Pages", type: :request do
       post user_google_oauth2_omniauth_authorize_url
     end
     it "GoogleのOAuth認証を通過していれば / へのアクセスが 200 で応答する" do
-      get root_url
+      # ここでは post user_google_oauth2_omniauth_authorize_url に対する
+      # レスポンスとして set_omniauth でモックされた「Google側の応答」が発生している。
+      # (byebug) response.body
+      # "Redirecting to http://www.example.com/users/auth/google_oauth2/callback..."
+      byebug
+
+      follow_redirect!
+
+      # ここでは controllers/users/omniauth_callbacks_controller.rb の Users::OmniauthCallbacksController#google_oauth2
+      # で redirect_to '/' しているのが反映されている。
+      # (byebug) response.body
+      # "<html><body>You are being <a href=\"http://www.example.com/\">redirected</a>.</body></html>"
+      byebug
+
+      follow_redirect!
+
+
+      # ここでは controllers/application_controller.rb の ApplicationController#connect_google_tasks で
+      # signet_oauth_client.fetch_access_token に失敗するために sign_out へのリダイレクトが発生している。
+      # (byebug) response.body
+      # "<html><body>You are being <a href=\"http://www.example.com/sign_out\">redirected</a>.</body></html>"
+      byebug
+
       expect(response).to have_http_status(200)
     end
   end
