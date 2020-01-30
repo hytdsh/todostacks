f00439a8cc81078a905170522d456837869effc3
hytdsh <hytdsh@gmail.com>
2020-01-30 10:15:18 +0900

    テストの流れについてコメント多めで整理した。

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -10,18 +10,31 @@ RSpec.describe "Pages", type: :request do
   context "OmniAuth" do
     before do
       set_omniauth
-      # it works without Rails.application.env_config[].
+      # it seems to work without Rails.application.env_config[].
 #      Rails.application.env_config["devise.mapping"] = Devise.mappings[:user]
 #      Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
     end
     subject do
+      # 「Google側」に post を送信する
       post user_google_oauth2_omniauth_authorize_url
-      follow_redirect! # モックされた「Google側の応答」がコールバックへのリダイレクトになっているので follow_redirect! が必要
+      # set_omniauth でモックされた「Google側の応答」がコールバックへのリダイレクトになっているので follow_redirect! が必要
+      follow_redirect!
     end
     it "GoogleのOAuth認証を通過すると User の count が 1 増えて、'/' へのアクセスが 200 になる" do
+      # コールバック Users::OmniauthCallbacksController の中で User が新規登録され count が1増える
       expect{ subject }.to change(User, :count).by(1)
-      #get root_path
-      #expect(response).to have_http_status(200)
+
+      # 次のステップで使う ApplicationController#connect_google_tasks をモックしているつもり
+      allow(controller).to receive(:connect_google_tasks).and_return(true)
+
+      # コールバックで「ユーザー登録 change(User, :count).by(1)」した次のステップとして '/' へのリダイレクトが
+      # 発生しているので follow_redirect! が必要
+      follow_redirect!
+      # follow_redirect! で '/' に遷移しようとして before_action で指定している　ApplicationController#connect_google_tasks
+      # を適切に構成できずにエラーが raise し、その結果として redirect_to destroy_user_session_path (つまり /sign_out) となる
+
+      # 前述した通り、現時点では response が 302 (/sign_out へのリダイレクト)となり fail する
+      expect(response).to have_http_status(200)
     end
   end
 
